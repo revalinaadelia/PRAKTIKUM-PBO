24b8758d27b35cbbc2df1d4464cf7a4ae1187e6c
revalinaadelia <144590332+revalinaadelia@users.noreply.github.com>
2024-09-26 18:23:01 +0700

    Binatang.java
    Source code untuk Binatang

---
+++ Binatang.java
@@ -0,0 +1,11 @@
+class Binatang {
+    protected String nama;
+
+    public Binatang(String nama) {
+        this.nama = nama;
+    }
+A
+    public void bersuara() {
+        System.out.println("Mbeek!");
+    }
+}
